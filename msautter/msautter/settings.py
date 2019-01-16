2df042d47424f521feb99265c3c964af28658c95
Marek Sautter <marek.sautter@gmail.com>
2019-01-15 16:46:08 -0800

    Added Django App

--- msautter/msautter/settings.py
+++ msautter/msautter/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'portfolio.apps.PortfolioConfig',
+    'smartcloset.apps.SmartclosetConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=(
+    os.path.join(BASE_DIR, 'assets'),
+)
--- msautter/msautter/urls.py
+++ msautter/msautter/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', include('portfolio.urls')),
@@ -22,3 +23,5 @@ urlpatterns = [
     path('smart-closet-app/', include('smartcloset.urls'))
 
 ]
+
+urlpatterns += staticfiles_urlpatterns()
